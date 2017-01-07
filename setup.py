2b81395e29fea410505d1d5e54dc3a6b15c5f98d
Arnab Kumar Shil <arnabkumarshil@Arnabs-MacBook-Pro.local>
2017-01-07 17:59:17 +0600

    added

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
   author_email='me@ruddra.com',
   license='MIT',
   url='https://github.com/ruddra/django-encrypt-file',
-  download_url='https://github.com/ruddra/django-encrypt-file/archive/0.2.tar.gz',
+  download_url='https://github.com/ruddra/django-encrypt-file/archive/0.4.tar.gz',
   keywords=['django', 'encryption', 'decrypt', 'decryption', 'encrypt', 'file', 'django file', 'upload', 'download', 'inmemoryfile', 'models.filefield'],
   classifiers=[],
-  platform = ['Windows', 'Linux', 'OSX'],
+  platform=['Windows', 'Linux', 'OSX'],
   install_requires=[
           'django>=1.7',
           'pycrypto>=2.6.1'
